7340052c533d7a4416d6beb2a490793ae56db7c7
Gagandeep Singh <gdss1109.com>
2019-08-25 19:20:20 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 /* this is first */
+
+$('#js-shopping-list-form').submit(function(event) {
+
+    // stop default submission
+    event.preventDefault();
+
+    // get text value for new item
+    const listItem = $('#shopping-list-entry').val();
+
+
+    // clear text input 
+    $('#shopping-list-entry').val('');
+
+    // add new shopping list item as li
+    $('.shopping-list').append(
+      `<li>
+      <span class="shopping-item">${listItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+  });
+
+// check and uncheck
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
+// removing 
+$('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
